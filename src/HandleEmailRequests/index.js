7c745ff0d674a45d36b3e217e3bd71d0d3fa1bc8
Chase Douglas <chase@stackery.io>
2019-04-24 10:19:54 -0700

    Add code!

--- src/HandleEmailRequests/index.js
+++ src/HandleEmailRequests/index.js
@@ -1,6 +1,74 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+const AWS = require('aws-sdk');
+const ses = new AWS.SES();
 
-  return {};
-};
+exports.handler = async message => {
+  console.log(message);
+
+  try {
+    const { to, subject, body } = validateParameters(message.body);
+
+    await ses.sendEmail({
+      Source: 'demo@stackery.io',
+      Destination: {
+        ToAddresses: [ to ]
+      },
+      Message: {
+        Subject: { Data: subject },
+        Body: {
+          Text: { Data: body }
+        }
+      }
+    }).promise();
+
+    return {
+      statusCode: 204
+    };
+  } catch (err) {
+    if (err.response) {
+      return err.response;
+    } else {
+      return {
+        statusCode: 500,
+        body: `${err.message} (${err.code})`
+      };
+    }
+  }
+}
+
+function validateParameters (rawBody) {
+  function error(message) {
+    const err = new Error(`Invalid Request: ${message}`);
+
+    err.response = {
+      statusCode: 400,
+      body: `Invalid Request: ${message}`
+    };
+
+    throw err;
+  }
+
+  if (!rawBody) {
+    error('Missing request body');
+  }
+
+  let body;
+  try {
+    body = JSON.parse(rawBody)
+  } catch (err) {
+    error('Request body must be valid JSON');
+  }
+
+  if (typeof body.to !== 'string') {
+    error('`to` must be an email address');
+  }
+
+  if (typeof body.subject !== 'string') {
+    error('`subject` must be a string');
+  }
+
+  if (typeof body.body !== 'string') {
+    error('`body` must be a string');
+  }
+
+  return ({ to, subject, body } = body);
+}
--- src/records/index.js
+++ src/records/index.js
@@ -1,6 +1,92 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 
-  return {};
-};
+exports.handler = async message => {
+  console.log(message);
+
+  try {
+    const recordId = message.pathParameters.id;
+    switch (message.httpMethod) {
+      case 'GET':
+        return getRecord(recordId);
+
+      case 'PUT':
+        let body;
+        try {
+          body = JSON.parse(message.body)
+        } catch (err) {
+          return {
+            statusCode: 400,
+            body: 'Request body must be valid JSON'
+          }
+        }
+
+      return putRecord(recordId, body);
+
+      case 'DELETE':
+        return deleteRecord(recordId)
+
+      default:
+        return {
+          statusCode: 405,
+          body: `Invalid HTTP method ${message.httpMethod}`
+        };
+    }
+  } catch (err) {
+    if (err.response) {
+      return err.response;
+    } else {
+      return {
+        statusCode: 500,
+        body: `${err.message} (${err.code})`
+      };
+    }
+  }
+}
+
+async function getRecord (id) {
+  const { Item } = await ddb.get({
+    TableName: process.env.TABLE_NAME,
+    Key: { id }
+  }).promise();
+
+  if (!Item) {
+    return {
+      statusCode: 404,
+      body: `Record ${id} does not exist`
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(Item, null, 200),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+}
+
+async function putRecord (id, attributes) {
+  await ddb.put({
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      ...attributes,
+      id
+    }
+  }).promise();
+
+  return {
+    statusCode: 204
+  };
+}
+
+async function deleteRecord (id) {
+  await ddb.delete({
+    TableName: process.env.TABLE_NAME,
+    Key: { id }
+  }).promise();
+
+  return {
+    statusCode: 204
+  };
+}
